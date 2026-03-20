cc7090925b89297ba41e2a442271539be5b5e357
Guilherme <macarioguilherme8534@gmail.com>
2026-03-20 19:18:05 -0300

    Commit!

---
+++ Section3/aula15.py
@@ -0,0 +1,6 @@
+#Começando a usar o input
+
+resposta = input("Qual é o seu nome: ")
+numero = input("Qual é o número: ")
+int_numero = int(numero)
+print(f"Olá {resposta} tudo bem?!, seu número escolhido foi {int_numero}")
