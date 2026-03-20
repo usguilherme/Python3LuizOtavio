c75a6d31151290ae99410edf3cc083376d2548fe
Guilherme <macarioguilherme8534@gmail.com>
2026-03-20 18:58:55 -0300

    Commit!

---
+++ Section3/aula04.py
@@ -0,0 +1,6 @@
+#Introdução á variaveis
+int_um = 1
+int_dois = 2
+idade = 30
+print(f'A sua idade é de {idade}')
+
---
+++ Section3/aula10.py
@@ -0,0 +1,9 @@
+#Concatenação
+
+concatencao = "Guilherme" + " " + "Macario"
+print(concatencao)
+
+dez_vezes = "Guilherme " * 10
+tres_vezes = "Guilherme " * 3
+print(dez_vezes)
+print(tres_vezes)
