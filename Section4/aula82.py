63c25330c197504582c52054acae35ae6e54fb08
Guilherme <macarioguilherme8534@gmail.com>
2026-04-06 19:14:46 -0300

    Commit!

--- Section4/aula82.py
+++ Section4/aula82.py
@@ -2,9 +2,25 @@
 #lambda argumentos: expressão
 
 soma = lambda x, y: x + y # funcao lambda, onde tenho x e y, e "retorno" x + y
-print(soma(2,5))
+#print(soma(2,5))
 
 numeros = [1,2,3,4,5,6,7,8]
 pares = filter(lambda x: x % 2 == 0, numeros)
 print(list(pares))
+
+#ordenar um array usando lambda
+numeros2 = [10,3218,1,5,4,31231,323]
+ordenado = sorted(numeros2, key = lambda x: x)
+print(ordenado)
+
+pessoas = [("João", 25), ("Maria", 20), ("Ana", 30)] #ordenar pela a idade deles
+
+pessoas_ordenadas = sorted(pessoas, key = lambda x: x[1]) #lambda argumentos: regra a ser ordenado
+print(pessoas_ordenadas)
+
+
+
+
+
+
 
