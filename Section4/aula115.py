1b0fb5239db2b343b270a0a532abdf0a4e79edb0
Guilherme <macarioguilherme8534@gmail.com>
2026-04-07 18:18:01 -0300

    Commit!

--- Section4/aula115.py
+++ Section4/aula115.py
@@ -1,3 +1,5 @@
 #Ambientes virtuais em Python (venv)
 #venv é o módulo que vou usar para criar ambientes virtuais
-
+#Um ambiente virtual carrega toda a sua instalação
+#do Python para uma pasta no caminho escolhido
+#nome geralmente usados: venv, env, .venv, .env
