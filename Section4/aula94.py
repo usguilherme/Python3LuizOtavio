95d5d142e8c0c684c6e65b96633896fa108e45d4
Guilherme <macarioguilherme8534@gmail.com>
2026-04-07 01:24:03 -0300

    Commit!

---
+++ Section4/aula94.py
@@ -0,0 +1,11 @@
+"""
+try - tento algo
+except - caso der erro no try
+finally - sempre vai acontecer 
+
+"""
+try: # tento
+    print("Abrir o arquivo")
+    10/0
+finally: #sempre será executado
+    print("Fechar arquivo")
