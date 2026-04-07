db0bb1db6ea374ddc40693945588f52dd4867ab6
Guilherme <macarioguilherme8534@gmail.com>
2026-04-07 15:31:34 -0300

    Commit!

---
+++ Section4/aula98.py
@@ -0,0 +1,20 @@
+from functools import reduce
+
+
+array = [1,2,3,4,5,6,7]
+array_novos_valores = [x ** 2 for x in array]
+
+
+#Usando função map, até com lambda, mapeando
+resultado = map(lambda x: x ** 2, array)
+#print(list(resultado))
+
+#usando a funcao filter, estou filtrando meu interável
+resultado1 = filter(lambda x: x % 2 == 0, array) #mostra apenas o números pares
+print(list(resultado1))
+
+#usando o reduce, reduzindo uma lista em um único valor
+resultado2 = reduce(lambda x,y: x + y, array)
+print(resultado2) #somatório de todos
+
+
