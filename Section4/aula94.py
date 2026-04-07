1e71ade791308a3e0d9c4f538aea9232dca5f7ba
Guilherme <macarioguilherme8534@gmail.com>
2026-04-07 01:25:07 -0300

    Commit!

--- Section4/aula94.py
+++ Section4/aula94.py
@@ -7,5 +7,8 @@ finally - sempre vai acontecer
 try: # tento
     print("Abrir o arquivo")
     10/0
+except ZeroDivisionError as erro:
+    print(f"Não é possível dividir um número por zero")
+    print(erro)
 finally: #sempre será executado
     print("Fechar arquivo")
