753084a8883fb86e34a29417f20ceb7e34a592fd
Guilherme <macarioguilherme8534@gmail.com>
2026-04-08 17:43:55 -0300

    Commit!

---
+++ Section4/aula119_desafio.py
@@ -0,0 +1,70 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
+
+
+import os
+
+
+def listar_tarefas(tarefas):
+    print("TAREFAS:\n")
+    if tarefas: #não sendo vázio
+        for i, tarefa  in enumerate(tarefas, 1):
+            print(f"{i}.{tarefa}")
+    else:
+        print("Nenhuma tarefa encontrada.")
+
+def desfazer(tarefas, tarefas_desfeitas):
+    if not tarefas: #não sendo vázia
+        print("Nada a desfazer")
+        return
+    ultimo_elemento = tarefas.pop() #removendo último elemento
+    tarefas_desfeitas.append(ultimo_elemento) #inserindo o último elemento no meu array de desfeitas
+
+def refazer(tarefas, tarefas_desfeitas):
+    if tarefas_desfeitas: #não sendo vázio
+        tarefas.append(tarefas_desfeitas[-1]) #inserir o último do tarefas_desfeitas
+        tarefas_desfeitas.pop() #removo o último do array
+    else:
+        print("Não foi possível desfazer: ")
+
+
+def adicionar(tarefa, tarefas):
+    print()
+    if not tarefa: #existe alguma tarefa
+        print("Não foi possível adicionar sua tarefa.")
+        return
+    
+    tarefa = tarefa.strip()
+    print(f"A tarefa {tarefa} foi adicionada com sucesso: ")
+    tarefas.append(tarefa)
+    print()
+
+tarefas = []
+tarefas_desfeitas = []
+while True:
+    print("Comandos: listar, desfazer, refazer")
+    tarefa = input("Digite uma tarefa ou comando: ").lower().strip()
+
+    if tarefa == 'listar':
+        listar_tarefas(tarefas)
+        continue
+    elif tarefa == 'desfazer':
+        desfazer(tarefas, tarefas_desfeitas)
+        continue
+    elif tarefa == 'refazer':
+        refazer(tarefas, tarefas_desfeitas)
+    elif tarefa == 'clear':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        continue
+    else:
+        adicionar(tarefa, tarefas)
+        listar_tarefas(tarefas)
+        continue    
