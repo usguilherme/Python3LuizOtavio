f5389c905252bea3f151e15e9d2054212599e303
Guilherme <macarioguilherme8534@gmail.com>
2026-05-29 13:20:01 -0300

    Commit!

---
+++ Section6/começandoOs.py
@@ -0,0 +1,11 @@
+import os
+
+
+print('g' * 80)
+print('g' * 80)
+print('g' * 80)
+print('g' * 80)
+print('g' * 80)
+
+os.system('cls') #limpar o terminal
+os.system('echo olá tudo bem?!') #os.system('echo alguma coisa') o sistema operacional fala isso
