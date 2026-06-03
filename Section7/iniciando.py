13a3756e28451ebba7cc0d21c6d87d87cbcb8ab5
Guilherme <macarioguilherme8534@gmail.com>
2026-06-03 19:13:59 -0300

    Commit!

--- Section7/iniciando.py
+++ Section7/iniciando.py
@@ -3,9 +3,20 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent #o caminho da pasta que estou agora
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME  
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE) #conectando o meu arquivo
 cursor = connection.cursor() #inciando o cursor
+
+cursor.execute(f'''
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        weight REAL
+    )
+''')
+
+connection.commit()
 
 
 cursor.close() #fechando o cursor
