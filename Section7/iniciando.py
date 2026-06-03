67b3ef4b1ed78ea9692f7d9cbafdae89737fa4c3
Guilherme <macarioguilherme8534@gmail.com>
2026-06-03 18:49:36 -0300

    Commit!

--- Section7/iniciando.py
+++ Section7/iniciando.py
@@ -4,7 +4,9 @@ ROOT_DIR = Path(__file__).parent #o caminho da pasta que estou agora
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME  
 
-con = sqlite3.connect(DB_FILE) #conectando o meu arquivo
-cursor = con.cursor() #inciando o cursor
+connection = sqlite3.connect(DB_FILE) #conectando o meu arquivo
+cursor = connection.cursor() #inciando o cursor
+
+
 cursor.close() #fechando o cursor
-con.close() #fechando a conexão
+connection.close() #fechando a conexão
